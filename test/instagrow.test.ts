d5f58bbc71244eab6b1cec450956e960146a8be7
Clement Allen <clementallen313@gmail.com>
2023-08-16 22:32:41 +0100

    Initial commit

---
+++ test/instagrow.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Instagrow from '../lib/instagrow-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/instagrow-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Instagrow.InstagrowStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
