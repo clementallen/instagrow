da709207e13c55033a422708b60ac756f5e001a1
Clement Allen <clement.allen@accenture.com>
2023-09-12 21:05:22 +0100

    Link up Lambda to topic events

--- lib/instagrow-stack.ts
+++ lib/instagrow-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ThingWithCert } from 'cdk-iot-core-certificates';
+import * as iot from '@aws-cdk/aws-iot-alpha';
+import * as iotActions from '@aws-cdk/aws-iot-actions-alpha';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class InstagrowStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,6 +29,22 @@ export class InstagrowStack extends cdk.Stack {
 
         new cdk.CfnOutput(this, 'Output-PrivKey', {
             value: privKey,
+        });
+
+        const func = new Function(this, 'trigger', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'index.handler',
+            code: Code.fromInline(`
+              exports.handler = (event) => {
+                console.log("It is test for lambda action of AWS IoT Rule.", event);
+              };`),
+            functionName: 'instagrow-trigger',
+        });
+
+        const rule = new iot.TopicRule(this, 'rule', {
+            sql: iot.IotSql.fromStringAsVer20160323("SELECT * FROM 'instagrow/received'"),
+            actions: [new iotActions.LambdaFunctionAction(func)],
+            topicRuleName: 'instagrow_lambda_trigger',
         });
     }
 }
